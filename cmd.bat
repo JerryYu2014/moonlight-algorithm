97f63ff12d2e66e4aec37f98764756a5f36e2e74
JerryYu2014 <648542313@qq.com>
2020-07-25 18:18:56 +0800

    ...

--- cmd.bat
+++ cmd.bat
@@ -2,3 +2,10 @@ npm init
 npm adduser
 npm whoami
 npm publish
+
+npm i typescript -D
+
+.\node_modules\.bin\tsc --init
+
+npm i mocha -D
+npm i chai -D
--- test/test.js
+++ test/test.js
@@ -1,3 +1,13 @@
-var fn = require('../src/index');
+// var fn = require('../src/index');
+// fn.print_liyc_hello('Hello world!');
 
-fn.print_liyc_hello('Hello world!');
+'use strict';
+const expect = require('chai').expect;
+const add = require('../dist/index').add;
+
+describe('moonlight-algorithm function test', () => {
+  it('should return 2', () => {
+    const result = add(1, 1);
+    expect(result).to.equal(2);
+  });
+});

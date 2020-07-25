f3246e85386f76d69094afd6c2427f7b8248aa5f
JerryYu2014 <648542313@qq.com>
2020-07-25 18:50:31 +0800

    ...

--- cmd.bat
+++ cmd.bat
@@ -1,3 +1,5 @@
+
+REM npm 模块初始化及发布
 npm init
 npm adduser
 npm whoami
@@ -7,5 +9,10 @@ npm i typescript -D
 
 .\node_modules\.bin\tsc --init
 
+REM Test
 npm i mocha -D
 npm i chai -D
+
+REM Code coverage
+npm i istanbul -D
+npm i coveralls -D

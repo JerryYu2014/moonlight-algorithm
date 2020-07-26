0c6b94007e03481f9e2cd8ddc653fbd35f53f6da
JerryYu2014 <648542313@qq.com>
2020-07-26 21:37:49 +0800

    ...

--- test/test.js
+++ test/test.js
@@ -1,13 +1,12 @@
-// var fn = require('../src/index');
-// fn.print_liyc_hello('Hello world!');
 
 'use strict';
-const expect = require('chai').expect;
-const add = require('../dist/index').add;
 
-describe('moonlight-algorithm function test', () => {
+const expect = require('chai').expect;
+const algorithm = require('../dist/index');
+
+describe('moonlight-algorithm function add test', () => {
   it('should return 2', () => {
-    const result = add(1, 1);
+    const result = algorithm.add(1, 1);
     expect(result).to.equal(2);
   });
 });

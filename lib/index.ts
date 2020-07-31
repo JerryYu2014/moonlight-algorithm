09048b7b628c976c03aa712e8c6dd683c7090d71
advoster <648542313@qq.com>
2020-07-31 15:42:52 +0800

    ...

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,6 @@
 export * from './sort';
 export * from './search';
 export * from './diff';
-export * from './data-bidirectional-binding';
+// export * from './data-bidirectional-binding';
+
+// export * from './data-bidirectional-binding/compile'

5baf3a636337c9972be830b0acc664a2671899ba
advoster <648542313@qq.com>
2020-08-19 12:00:15 +0800

    ...

--- webpack.npm.config.js
+++ webpack.npm.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    // library: 'moonlight-algorithm',
+    library: 'moonlight-algorithm',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
